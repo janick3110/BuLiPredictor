f0c9628a516486413647987bae4ce94668fb1671
janick3110 <janick3110@gmail.com>
2022-08-16 12:03:49 +0200

    Updated a few games and more coding

--- Code/predictor.R
+++ Code/predictor.R
@@ -1,16 +1,17 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(melt)
+#library(melt)
+
 
 teamsForSeason <- c(
   "1. FC Köln",
   "1. FC Union Berlin",
   "1. FSV Mainz 05",
   "TSG 1899 Hoffenheim",
-  "Bayer Leverkusen",
+  "Bayer 04 Leverkusen",
   "Bayern München",
-  "Bor. Möchengladbach",
+  "Bor. Mönchengladbach",
   "Borussia Dortmund",
   "Eintracht Frankfurt",
   "FC Augsburg",
@@ -30,14 +31,49 @@ matches <- read.csv("../data/games.csv", sep=";")
 filteredMatchesHome <- matches %>% filter(Team.1 %in% teamsForSeason)
 filteredMatchesAway <- matches %>% filter(Team.2 %in% teamsForSeason)
 
-goalsShotHome <- aggregate(matches$Home, by=list(Club=matches$Team.1,matches$Startjahr), FUN=sum)
-goalsShotAway <- aggregate(matches$Guest, by=list(Club=matches$Team.2,matches$Startjahr), FUN=sum)
-goalsConcededHome <- aggregate(matches$Home, by=list(Club=matches$Team.2,matches$Startjahr), FUN=sum)
-goalsConcededAway <- aggregate(matches$Guest, by=list(Club=matches$Team.1,matches$Startjahr), FUN=sum)
+goalsShotHome <- aggregate(matches$Home, by=list(Club=matches$Team.1), FUN=sum)
+avgGoalsShotHome <- aggregate(matches$Home, by=list(Club=matches$Team.1), FUN=mean)
+goalsShotAway <- aggregate(matches$Guest, by=list(Club=matches$Team.2), FUN=sum)
+avgGoalsShotAway <- aggregate(matches$Guest, by=list(Club=matches$Team.2), FUN=mean)
+goalsConcededHome <- aggregate(matches$Home, by=list(Club=matches$Team.2), FUN=sum)
+avgGoalsConcededHome <- aggregate(matches$Home, by=list(Club=matches$Team.2), FUN=mean)
+goalsConcededAway <- aggregate(matches$Guest, by=list(Club=matches$Team.1), FUN=sum)
+avgGoalsConcededAway <- aggregate(matches$Guest, by=list(Club=matches$Team.1), FUN=mean)
 
 matchesOfCurrentTeams <- rbind(filteredMatchesHome,filteredMatchesAway)
 matchesOfCurrentTeams <- unique(matchesOfCurrentTeams)
 
+matches <- matches %>% mutate(homeVic =
+                                      case_when(Home > Guest ~ 1, 
+                                                  Home == Guest ~ 0,
+                                                  Home < Guest ~ 0)) %>% mutate(AwayVic =
+                                       case_when(Home > Guest ~ 0, 
+                                                 Home == Guest ~ 0,
+                                                 Home < Guest ~ 1)) %>% mutate(Equal =
+                                       case_when(Home > Guest ~ 0, 
+                                                 Home == Guest ~ 1,
+                                                 Home < Guest ~ 0))
+
+possibiltyOfHomeVictory <- sum(matches$homeVic)/nrow(matches)
+possibiltyOfGuestVictory <- sum(matches$AwayVic)/nrow(matches)
+possibiltyOfEqual <- sum(matches$Equal)/nrow(matches)
+
+clubs <- data.frame(
+  goalsShotHome$Club,
+  goalsShotHome$x,
+  avgGoalsShotHome$x,
+  goalsShotAway$x,
+  avgGoalsShotAway$x,
+  goalsConcededHome$x,
+  avgGoalsConcededHome$x,
+  goalsConcededAway$x,
+  avgGoalsConcededAway$x
+)
+
+clubs <- clubs %>% filter(goalsShotHome.Club %in% teamsForSeason)
+
+meanShotGoals <- mean(matches$Home)
+meanConcededGoals <- mean(matches$Guest)
 
 
 season2018 <- matchesOfCurrentTeams %>% filter(Startjahr == 2018)
@@ -45,41 +81,9 @@ season2019 <- matchesOfCurrentTeams %>% filter(Startjahr == 2019)
 season2020 <- matchesOfCurrentTeams %>% filter(Startjahr == 2020)
 
 
-melt_results <- function(results_df) {
-  results_df %>%
-    # select only relevant columns
-    select(Team.1, Team.2,Home, Guest) %>%
-    gather(location, team,-Home, -Guest) %>%
-    # calculate goals for/against the team
-    mutate(g_for = case_when(
-      location == "home" ~ Home,
-      location == "away" ~ Guest
-    )) %>%
-    mutate(g_ag = case_when(
-      location == "home" ~ Guest,
-      location == "away" ~ Home
-    )) 
-}
-
-goalData <- matches %>%
-  select(Team.1, Team.2 = visitor, Home, Away = vgoal) %>%
-  # munge
-  melt_results() %>%
-  select(-hgoal, -agoal) %>%
-  mutate(data = "real")
-
-goalData <- matches %>%
-  select(Team.1,Team.2,Guest, Home) %>% 
-  melt_results() %>%
-  mutate(data = "real")
-
 goalsOverview <- ggplot(matches) +
-  geom_density(adjust = 8, alpha = 0.5,aes(x = Home))+
-  geom_density(adjust = 8, alpha = 0.5,aes(x = Guest))+
-  scale_fill_manual(values = c("red", "blue")) +
-  scale_x_continuous(breaks = 0:8)
+  geom_density(adjust = 8, alpha = 0.5,aes(x=Home)) + geom_density(adjust = 8, alpha = 0.5,aes(x=Guest))
+
 goalsOverview
-
-
 
 
